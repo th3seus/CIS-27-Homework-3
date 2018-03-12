0a75bd038e73138f4273bcfe9513ddc229ecbff6
Stephen Miller <stephenmiller@fb.com>
2018-03-12 15:00:25 -0700

    Initial Commit

---
+++ cis27Spring2018StephenMHw3Ex1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cis27Spring2018StephenMHw3Ex1
+//
+//  Created by Stephen Miller on 3/12/18.
+//  Copyright © 2018 Stephen Miller. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
